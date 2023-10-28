a329701a2acf9c78daa899abfdce7d75e87535e1
Santiago-1505 <santiagovillegas853@gmail.com>
2023-10-28 16:31:48 -0500

    Se completo de manera exitosa, la segunda parte del laboratorio.

--- src/main/java/org/example/ApplicationRunner.java
+++ src/main/java/org/example/ApplicationRunner.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.example.exception.GradeNotFoundException;
 import org.example.model.Grade;
 import org.example.repository.GradeInMemoryRepositoryImpl;
+import org.example.repository.GradeUsingFileRepositoryImpl;
 import org.example.service.AcademicRecordService;
 import org.example.service.AcademicRecordServiceImpl;
 
@@ -15,35 +16,34 @@ public class ApplicationRunner {
   public static void main(String[] args) {
 
     AcademicRecordService academicRecordService =
-        new AcademicRecordServiceImpl(new GradeInMemoryRepositoryImpl());
+            new AcademicRecordServiceImpl(new GradeUsingFileRepositoryImpl());
 
-    //Disclaimer: Este metodo no sigue el principio SOLID de single responsibility
-    System.out.println("Notas iniciales");
-    List<Grade> gradeList1 =   academicRecordService.listAllGrades();
-    gradeList1.forEach( System.out::println );//Impresion en estilo de programacion funcional
+    //Disclaimer: Este metodo sigue el principio SOLID de single responsibility
+    mostrarNotas("Notas iniciales", academicRecordService.listAllGrades());
 
+    Grade grade = new Grade("PARCIAL", 4.5D, LocalDate.now());
+    academicRecordService.addGrade(grade);
 
-    Grade grade = new Grade( "PARCIAL", 4.5D, LocalDate.now() );
-    academicRecordService.addGrade( grade );
+    mostrarNotas("Notas después de adicionar una nueva",academicRecordService.listAllGrades());
 
+    mostrarResultado("Suma de número calificaciones", academicRecordService.sumNumberOfGrades());
+    mostrarResultado("Promedio", academicRecordService.calculateAverage());
 
-    System.out.println("Notas despues de adicionar una nueva");
-    List<Grade> gradeList2 =   academicRecordService.listAllGrades();
-    gradeList2.forEach( System.out::println );//Impresion en estilo de programacion funcional
-
-
-    System.out.println(
-            MessageFormat.format(
-                    "Suma de número calificaciones: {0}", academicRecordService.sumNumberOfGrades()));
-
-    System.out.println(
-            MessageFormat.format("Promedio: {0}", academicRecordService.calculateAverage()));
-
-
-    System.out.println("Consulta una nota de un proyecto que no existe");//El siguiente codigo debe generar una excepcion con el mensaje de "No se encontro"
     String nombreProyecto = "Unidad 10";
+    busquedaNota(academicRecordService, nombreProyecto);
+  }
+  private static void mostrarNotas (String mensaje, List<Grade>gradeList){
+      System.out.println(mensaje);
+      gradeList.forEach(System.out::println);
+  }
+  private static void mostrarResultado (String mensaje, Object resultado){
+      System.out.println(MessageFormat.format("{0}: {1}", mensaje, resultado));
+  }
+
+  private static void busquedaNota (AcademicRecordService academicRecordService, String nombreProyecto){
     try {
-      System.out.println(academicRecordService.getGrade( nombreProyecto ));
+      Grade grade = academicRecordService.getGrade( nombreProyecto );
+      System.out.println("Nota del proyecto '" + nombreProyecto + "': " + grade);
     }
     catch (GradeNotFoundException e) {
       System.out.println(MessageFormat.format("No se encontró una nota para la unidad {0} ", nombreProyecto));
